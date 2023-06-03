2e717004a84045bf0758fdf1a2dec92a160b3f48
Joseph Nyahuye <hotslices7@gmail.com>
2023-06-03 16:50:03 +0200

    styling fix

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -4,7 +4,7 @@ export default function Skeleton({ isSiteLevel = true }: Props) {
     return (
         <div className="min-h-screen bg-white flex flex-col justify-start items-center">
             {isSiteLevel && <div className="h-[65px] w-full bg-gray-300 animate-pulse"></div>}
-            <div className="w-full grid sm:grid-cols-2 gap-5 mx-auto px-8 mt-10">
+            <div className="w-full grid sm:grid-cols-2 gap-5 container mx-auto py-10 px-4">
                 <div className="w-full min-h-screen flex flex-col justify-between items-center gap-3 mx-0">
                     <div className="h-[20%] w-full flex justify-between items-center gap-3">
                         <div className="bg-gray-300 h-full w-1/3 animate-pulse"></div>
